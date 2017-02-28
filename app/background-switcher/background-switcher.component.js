075a4dde72d1d568175681ad5a73c14f0283af50
Basil Dudka <dudka.vasia2010@gmail.com>
2017-02-28 23:36:40 +0200

    working version of switcher

--- app/background-switcher/background-switcher.component.js
+++ app/background-switcher/background-switcher.component.js
@@ -5,8 +5,13 @@ angular.
   module('bgSwitch').
   component('bgSwitch', {
     templateUrl: 'background-switcher/background-switcher.template.html',
-    controller: function bgSwitchController($http) {
+    controller: function bgSwitchController($http, $rootScope) {
+      $rootScope.mainBackgroundUrl = 'img/backgrounds/elitefon.ru_28513.jpg';
       var self = this;
+
+      self.setBackground = function setBackground(source) {
+        $rootScope.mainBackgroundUrl = source;
+      }
 
       $http.get('img/backgrounds.json').then(function(response) {
         self.backgrounds = response.data;
