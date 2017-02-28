fd6d6a57136e71d3888b51f09e757f85de0f9a9c
Basil Dudka <dudka.vasia2010@gmail.com>
2017-03-01 00:58:34 +0200

    improved button, added hide/show effect

--- app/background-switcher/background-switcher.component.js
+++ app/background-switcher/background-switcher.component.js
@@ -9,8 +9,14 @@ angular.
       $rootScope.mainBackgroundUrl = 'img/backgrounds/landscape-nature-sunset-trees_C2bOWoK.jpg';
       var self = this;
 
+      self.isShown = false;
+
       self.setBackground = function setBackground(source) {
         $rootScope.mainBackgroundUrl = source;
+      }
+
+      self.toggleMenu = function toggleMenu() {
+        self.isShown=!self.isShown;
       }
 
       $http.get('img/backgrounds.json').then(function(response) {
