d0d3834151d8d87d5f0a688f89aaf5671c809e6d
Basil Dudka <dudka.vasia2010@gmail.com>
2017-03-02 10:15:49 +0200

    removeCompleted function

--- app/todo-list/todo-list.component.js
+++ app/todo-list/todo-list.component.js
@@ -11,19 +11,28 @@ angular.
         {
           desription: 'lorem ipsum dolar',
           priority: 'High',
-          done: false
+          completed: false
         },
         {
           desription: 'lodar ipsum lorem',
           priority: 'Low',
-          done: false
+          completed: false
         }
       ];
 
       this.addTask = function(task) {
-        task.done = false;
+        task.completed = false;
         this.tasks.push(task);
         this.currentTask = {};
-      }
+      };
+
+      this.removeCompleted = function() {
+        var temp = [];
+        for (var i = 0; i < this.tasks.length; i++) {
+          if(!this.tasks[i].completed)
+            temp.push(this.tasks[i]);
+        }
+        this.tasks = temp;
+      };
     }
   });
